493c5306fb2463246b881119604f54311a39f460
Roman Böhringer <r.boehringer@opencore.ch>
2023-02-09 11:59:00 +0100

    Zalgo

--- src/StringImageUtils.sol
+++ src/StringImageUtils.sol
@@ -8,6 +8,20 @@ library StringImageUtils {
     bytes constant FONT_SQUIGGLE =
         hex"CEB1E182A6C688D483D2BDCF9DC9A0D48BCEB9CA9DC699CA85C9B1C9B3CF83CF81CF99C9BECA82C69ACF85CA8BC9AF78E183A7C8A5";
 
+    bytes constant ZALGO_ABOVE_LETTER =
+        hex"CC80CC81CC82CC83CC84CC85CC86CC87CC88CC89CC8ACC8BCC8CCC8DCC8ECC8FCC90CC91CC92CC93CC94CC95CC9ACC9BCCBDCCBECCBFCD80CD81CD82CD83CD84CD86CD8ACD8BCD8CCD90CD91CD92CD97CD98CD9BCD9DCD9ECDA0CDA1";
+
+    uint256 constant ZALGO_NUM_ABOVE = 46;
+
+    bytes constant ZALGO_BELOW_LETTER =
+        hex"CC96CC97CC98CC99CC9CCC9DCC9ECC9FCCA0CCA1CCA2CCA3CCA4CCA5CCA6CCA7CCA8CCA9CCAACCABCCACCCADCCAECCAFCCB0CCB1CCB2CCB3CCB9CCBACCBBCCBCCD85CD87CD88CD89CD8DCD8ECD93CD94CD95CD96CD99CD9ACD9CCD9FCDA2";
+
+    uint256 constant ZALGO_NUM_BELOW = 47;
+
+    bytes constant ZALGO_OVER_LETTER = hex"CCB4CCB5CCB6CCB7CCB8";
+
+    uint256 constant ZALGO_NUM_OVER = 5;
+
     /// @notice Convert a given font class, character index, and a seed (for font classes with randomness) to their Unicode representation as bytes
     /// @param _fontClass The class to convert
     /// @param _characterIndex Index within the class
@@ -29,6 +43,45 @@ library StringImageUtils {
             return abi.encodePacked(bytes1(asciiStartingIndex + uint8(_characterIndex)));
         } else if (_fontClass == 7) {
             // Zalgo
+            uint8 asciiStartingIndex = 48;
+            if (_characterIndex > 9) {
+                asciiStartingIndex = 87;
+            }
+            uint256 numAbove = (_seed % 7) + 1;
+            // We do not reuse the same seed for the following generations to avoid any symmetries, e.g. that 2 chars above would also always result in 2 chars below
+            _seed = _iteratePRNG(_seed);
+            uint256 numMiddle = _seed % 2;
+            _seed = _iteratePRNG(_seed);
+            uint256 numBelow = (_seed % 7) + 1;
+            bytes memory character = abi.encodePacked(bytes1(asciiStartingIndex + uint8(_characterIndex)));
+            for (uint256 i; i < numAbove; ++i) {
+                _seed = _iteratePRNG(_seed);
+                uint256 characterIndex = (_seed % ZALGO_NUM_ABOVE) * 2;
+                character = abi.encodePacked(
+                    character,
+                    ZALGO_ABOVE_LETTER[characterIndex],
+                    ZALGO_ABOVE_LETTER[characterIndex + 1]
+                );
+            }
+            for (uint256 i; i < numMiddle; ++i) {
+                _seed = _iteratePRNG(_seed);
+                uint256 characterIndex = (_seed % ZALGO_NUM_OVER) * 2;
+                character = abi.encodePacked(
+                    character,
+                    ZALGO_OVER_LETTER[characterIndex],
+                    ZALGO_OVER_LETTER[characterIndex + 1]
+                );
+            }
+            for (uint256 i; i < numBelow; ++i) {
+                _seed = _iteratePRNG(_seed);
+                uint256 characterIndex = (_seed % ZALGO_NUM_BELOW) * 2;
+                character = abi.encodePacked(
+                    character,
+                    ZALGO_BELOW_LETTER[characterIndex],
+                    ZALGO_BELOW_LETTER[characterIndex + 1]
+                );
+            }
+            return character;
         } else {
             // TODO: Numbers that do not have a symbol
             uint24 unicodeStartingIndex;
@@ -98,5 +151,15 @@ library StringImageUtils {
                 textData,
                 "</text></svg>"
             );
+    }
+
+    /// @notice Simple PRNG to generate new numbers based on the current state
+    /// @param _currState Current state of the PRNG (initially the seed)
+    /// @return iteratedState New number
+    function _iteratePRNG(uint256 _currState) private pure returns (uint256 iteratedState) {
+        unchecked {
+            iteratedState = _currState * 15485863;
+            iteratedState = iteratedState * iteratedState * iteratedState;
+        }
     }
 }
