a2e74440ee04864a490e6cf07af2a7325fb61527
Roman Böhringer <r.boehringer@opencore.ch>
2023-02-16 14:09:53 +0100

    Comments

--- src/Tray.sol
+++ src/Tray.sol
@@ -226,7 +226,8 @@ contract Tray is ERC721A, Owned {
         uint256 numPrelaunchMinted = prelaunchMinted;
         if (numPrelaunchMinted != type(uint256).max) {
             // We do not allow any transfers of the prelaunch trays after the phase has ended
-            if (startTokenId <= numPrelaunchMinted && to != address(0)) // TODO: Still allows fusing
+            if (startTokenId <= numPrelaunchMinted && to != address(0))
+                // TODO: Still allows fusing
                 revert PrelaunchTrayCannotBeUsedAfterPrelaunch(startTokenId);
         }
     }
